21efc2fc782f52da1361ef7379ea2cca1398a8c9
JuliaCMS <julia.cms@outlook.com>
2025-02-02 21:30:48 -0300

    Estruturas de repetição

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -70,10 +70,159 @@
 
 // Calculadora
 
-Calculadora calculadora = new Calculadora();
+// Calculadora calculadora = new Calculadora();
 
-calculadora.Somar(3, 5);
-calculadora.Subtrair(8, 4);
-calculadora.Multiplicar(2, 8);
-calculadora.Dividir(18, 9);
-calculadora.Potencia(2, 6);
+// calculadora.Somar(3, 5);
+// calculadora.Subtrair(8, 4);
+// calculadora.Multiplicar(2, 8);
+// calculadora.Dividir(18, 9);
+// calculadora.Potencia(2, 6);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Estruturas de repetição
+
+// FOR
+// int numero = 5;
+
+// for (int i = 1; i <= 10; i++)
+// {
+//     int multiplicacao = numero * i;
+//     System.Console.WriteLine($"{numero} X {i} = {multiplicacao}");
+// }
+
+// WHILE
+
+// int numero = 5;
+// int i = 1;
+
+// while (i <= 10) 
+// {
+//     int multiplicacao = numero * i;
+//     Console.WriteLine($"{numero} x {i} = {multiplicacao}");
+
+//     i++;
+
+//     if (i == 6) // quero que vá até a quinta execução
+//     {
+//         break; // sai do laço
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DO WHILE
+
+// int numero = 5;
+// int i = 1;
+
+// do
+// {
+//     int multiplicacao = numero * i;
+//     System.Console.WriteLine($"{numero} x {i} = {multiplicacao}");
+
+//     i++;
+// } while (i <= 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MENU INTERATIVO
+
+int opcao;
+bool continuar = true;
+
+while (continuar)
+{
+    System.Console.WriteLine("OPÇÕES");
+    Console.WriteLine("Opçao 1 - Cadastrar cliente.");
+    Console.WriteLine("Opçao 2 - Buscar cliente.");
+    Console.WriteLine("Opçao 3 - Apagar cliente.");
+    Console.WriteLine("Opçao 4 - Encerrar.");
+    Console.Write("Digite a sua opção: ");
+    opcao = Convert.ToInt32(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            Console.WriteLine("Cadastrar Cliente.");
+            break;
+
+        case 2:
+            Console.WriteLine("Buscar cliente.");
+            break;
+
+        case 3:
+            Console.WriteLine("Apagar Cliente.");
+            break;
+
+        case 4:
+            Console.WriteLine("Encerrar.");
+            continuar = false;
+            break;
+
+        default:
+            System.Console.WriteLine("Opcao Inválida");
+            break;
+    }
+}
